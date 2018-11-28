799354bec2f9e2ec5fe068f177a2256bad25566b
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-11-28 11:15:17 +0200

    Fixed config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 const Config = {
-	middlewareHost: "http://192.168.1.141:9000",
-	middlewareOriginHost: "http://testnet.bonex.io:9000",
+	middlewareHost: "http://testnet.bonex.io:9000",
 	stellarNetwork: "Bonex Network ; October 2018",
 	horizonHost: "http://testnet.bonex.io:8000",
 	masterSeed: "SBNUB7DZTQESHSUQLBPSKRAADVX53VQDNRDJIE3CKTM6RSPI23R322IZ",
